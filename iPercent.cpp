f244cde7dfe2bee3a68cf987928a2d7043392fb3
NIKITA BORSE <nikita.borse@cumminscollege.in>
2014-09-12 09:23:52 +0530

    We made some changes..
    Apurwa got added in the group..

--- iPercent.cpp
+++ iPercent.cpp
@@ -6,5 +6,6 @@ int main()
 cout<<"Hello Nish n Praj"<<endl;
 cout<<"we both are so beautiful"<<endl;
 cout<<"OUR GROUP IS GOOD"<<endl;
+cout<<"This is apurwa saying hello to nishu and praju..!!"
 return 0;
 }
